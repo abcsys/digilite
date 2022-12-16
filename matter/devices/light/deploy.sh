faffdf4156dc2a4fad6c3a8f8d15894d6ea3608c
PotatoParser <wilsonqnguyen@gmail.com>
2022-12-15 22:06:48 -0800

    Upgrade virtual devices

--- matter/devices/light/deploy.sh
+++ matter/devices/light/deploy.sh
@@ -5,7 +5,7 @@ MINIKUBE_IP=$(minikube ssh grep host.minikube.internal /etc/hosts | cut -f1 -s -
 docker build -t ${MINIKUBE_IP}:5000/matter-light .
 docker image tag ${MINIKUBE_IP}:5000/matter-light localhost:5000/matter-light
 docker push localhost:5000/matter-light
-kubectl delete -f ../../util/external-mdns.yml
-kubectl apply -f ../../util/external-mdns.yml
-kubectl delete -f light.yml
+kubectl delete --all -f light.yml
+kubectl delete --all -f ../../util/external-mdns.yml
 kubectl apply -f light.yml
+kubectl apply -f ../../util/external-mdns.yml
